4350b8786465b7593ca5830b5c6503ccb34def5d
RUHULAMIN2024 <uhulamin15jun1999@gmail.com>
2024-02-19 08:05:35 +0600

    first commit

--- script.js
+++ script.js
@@ -30,19 +30,43 @@ function showElementById(elementId) {
     element.classList.remove('hidden');
 }
 
+
+
 let seatLeft= getTextElementValueById('seatLeft');
 const seats = document.getElementsByClassName("seat");
+const tbody = document.getElementById("tbody");
 let count= 0;
 for(let seat of seats){
     
     
     seat.addEventListener('click', function(){
         count +=1;
-        seatLeft -=1;
-        setTextElementValueById('seatLeft', seatLeft)
+
+
+        
+        
         if(count<=4){
+            
+            seatLeft -=1;
+            setTextElementValueById('seatLeft', seatLeft)
             const seatId= seat.innerText;
             setBackgroundColorById(seatId);
+
+            setTextElementValueById('selectedSeat', count)
+
+            let tr=document.createElement('tr');
+            let td1=document.createElement('td');
+            td1.innerText=seat.innerText;
+            let td2=document.createElement('td');
+            td2.innerText='Economey'
+            let td3=document.createElement('td');
+            td3.innerText=550;
+
+            
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tbody.appendChild(tr);
 
         }
         return
